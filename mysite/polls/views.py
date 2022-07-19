7eb34ef8e0c72477076b070dbc12337205953dd7
Hazel <forest@handong.ac.kr>
2022-07-19 14:02:57 +0900

    “commit”

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
 
-def index(req):
-    return HttpResponse("polls")
+
+def index(request):
+    latest_question_list = models.Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(req, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(req, question_id):
+    response = "You're looking at the results of question {0}. {1} {0}"
+    return HttpResponse(response.format(question_id,"aaa"))
+
+def vote(req, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
